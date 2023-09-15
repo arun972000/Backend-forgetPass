0ea3228a3710ec62acaab81a36b0fd254e0f22b2
arun972000 <128138897+arun972000@users.noreply.github.com>
2023-09-15 13:04:03 +0530

    Update app-user.js

--- Routes/app-user.js
+++ Routes/app-user.js
@@ -38,6 +38,7 @@ userRoutes.post('/resetPass', async (req, res) => {
         const isUser = await usermodel.findOne({ email: payload.email }, { id: 1, name: 1, email: 1, password: 1 })
         if(isUser){
             const token = jwt.sign({ email: payload.email }, process.env.JWT_KEY, { expiresIn: "1hr" })
+            const addlink= await usermodel.updateOne({email:payload.email},{$set:{verify_link:token}})
             const link = `${process.env.HOST}verifypass?token=${token}`
             transporter.sendMail({ ...mailOptions, to: payload.email, text: `please verify your email ${link}` }, function (error, info) {
                 if (error) {
